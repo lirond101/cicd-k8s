00d0bcaed77eb04c2f645c43e2286454dc6e5940
lirond101 <lirond101@gmail.com>
2023-08-27 14:39:14 +0000

    fix of cd jenkinsfile

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 AWS_REGION="us-east-2"
 
-# echo "Executing terraform vpc" 
-# cd ~/cicd-k8s/infrastructure/aws/vpc/
-# terraform init
-# terraform apply --auto-approve
+echo "Executing terraform vpc" 
+cd ~/cicd-k8s/infrastructure/aws/vpc/
+terraform init
+terraform apply --auto-approve
 
 echo "Executing terraform eks" 
 cd ~/cicd-k8s/infrastructure/aws/eks/
-# terraform init
-# terraform apply --auto-approve
+terraform init
+terraform apply --auto-approve
 CLUSTER_NAME=$(terraform output -raw cluster_name)
 
 echo "Provisioning kubernetes cluster $CLUSTER_NAME on $AWS_REGION"
